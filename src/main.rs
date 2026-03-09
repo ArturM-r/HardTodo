cebedb0ec1a31df74b5e06db9b1dcb30e61b50d2
Артур Миллер <arturmiller@MacBook-Air-Artur.local>
2026-03-09 03:55:34 +0300

    logging

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,19 @@ use tokio::net::TcpListener;
 use crate::db::Db;
 use crate::handlers::{create, delete_one, get_all, get_one, update};
 use crate::modules::AppState;
-
-
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| EnvFilter::new("info")),
+        )
+        .init();
+
     let db = Db::new();
     let state = Arc::new(AppState{db });
 
@@ -25,6 +33,7 @@ async fn main() {
         .route("/todo/{id}", get(get_one))
         .route("/todo/{id}", put(update))
         .route("/todo/{id}", delete(delete_one))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
